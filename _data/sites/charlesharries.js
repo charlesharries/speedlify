58ff153c4fe42b8681738ac3b462d943ff638ebd
Charles Harries <charles@harries.me>
2022-04-15 10:40:33 +0100

    Add personal site

---
+++ _data/sites/charlesharries.js
@@ -0,0 +1,18 @@
+module.exports = {
+	name: "charlesharri.es", // optional, falls back to object key
+	description: "Charles Harries's personal site",
+	skip: false
+	options: {
+		frequency: 60 * 23, // 23 hours
+		// Use "run" if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use "site" if sites are all on the same origin and share assets.
+		freshChrome: "site"
+	},
+	urls: [
+		"https://charlesharri.es",
+		"https://charlesharri.es/blog",
+		"https://charlesharri.es/stream",
+	]
+};
